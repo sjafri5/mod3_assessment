8a87bf9164506d0b4f932a9856a91dffa3e40415
sjafri5 <raza.jafri.sy@gmail.com>
2019-11-27 11:28:39 -0600

    asfasdf

---
+++ src/index.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+
+  let imageId = 1 //Enter the id from the fetched image here
+
+
+
+  console.log('asdf')
+
+  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+
+  const likeURL = `https://randopic.herokuapp.com/likes/`
+
+  const commentsURL = `https://randopic.herokuapp.com/comments/`
+
+})
